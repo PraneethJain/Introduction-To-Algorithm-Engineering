3ba850c900343f1c704d5366db966361bc823456
PraneethJain <praneethjain005@gmail.com>
2024-03-24 19:04:06 +0530

    feat: square matrix times

---
+++ Homework-2/square.sh
@@ -0,0 +1,10 @@
+g++ -O1 ./transpose.cpp
+for i in {1..3}
+do
+    for n in {100..10000..100}
+    do
+        echo -n "$n," >> "square_${i}.txt"
+        ./a.out $n $n $i >> "square_${i}.txt"
+    done
+done
+rm ./a.out
