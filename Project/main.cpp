b9c4594de567b494b31d9a9c285ca46ad739e131
PraneethJain <praneethjain005@gmail.com>
2024-04-07 17:54:39 +0530

    feat: tarjan biconnected components

--- Project/main.cpp
+++ Project/main.cpp
@@ -1,5 +1,6 @@
 #include "include/schmidt.hpp"
 #include "include/tarjan.hpp"
+#include <fstream>
 #include <iostream>
 
 template <typename T1, typename T2> std::ostream &operator<<(std::ostream &os, const std::pair<T1, T2> &pair)
@@ -8,17 +9,35 @@ template <typename T1, typename T2> std::ostream &operator<<(std::ostream &os, c
   return os;
 }
 
+Graph read_graph(std::string filename)
+{
+  std::ifstream file{filename};
+
+  int n{}, m{};
+  file >> n >> m;
+  Graph g(n, std::vector<int>{});
+  for (int i{0}; i < m; ++i)
+  {
+    int u{}, v{};
+    file >> u >> v;
+    g[u].push_back(v);
+    g[v].push_back(u);
+  }
+
+  return g;
+}
+
 int main()
 {
-  std::cout << "test" << std::endl;
-  Graph g{{1, 2}, {0, 2}, {0, 1}};
+  Graph g{read_graph("test.in")};
   BCC a{tarjan(g)}, b{schmidt(g)};
   for (Edges es : a)
   {
     for (Edge e : es)
     {
-      std::cout << e << std::endl;
+      std::cout << e << " ";
     }
+    std::cout << std::endl;
   }
 
   return 0;
--- Project/tarjan.cpp
+++ Project/tarjan.cpp
@@ -1,9 +1,75 @@
 #include "include/tarjan.hpp"
+#include <stack>
+
+int disc_time{0};
+
+Edges make_edges(std::stack<Edge> &stack, const Edge stop_edge)
+{
+  Edges es{};
+  while (!stack.empty() && stack.top() != stop_edge)
+  {
+    es.emplace(stack.top());
+    stack.pop();
+  }
+
+  if (!stack.empty())
+  {
+    es.emplace(stack.top());
+    stack.pop();
+  }
+
+  return es;
+}
+
+void DFS(const Graph &g, int u, std::vector<int> &low, std::vector<int> &disc, std::vector<int> &par,
+         std::stack<Edge> &stack, BCC &bcc)
+{
+  disc[u] = low[u] = ++disc_time;
+  int children{0};
+
+  for (int v : g[u])
+  {
+    if (disc[v] == -1)
+    {
+      ++children;
+      stack.emplace(u, v);
+      par[v] = u;
+      DFS(g, v, low, disc, par, stack, bcc);
+      low[u] = std::min(low[u], low[v]);
+
+      if ((par[u] == -1 && children > 1) || (par[u] != -1 && low[v] >= disc[u]))
+      {
+        bcc.insert(make_edges(stack, {u, v}));
+      }
+    }
+    else if (par[u] != v)
+    {
+      low[u] = std::min(low[u], disc[v]);
+      if (disc[v] < disc[u])
+      {
+        stack.emplace(u, v);
+      }
+    }
+  }
+}
 
 BCC tarjan(const Graph &g)
 {
+  disc_time = 0;
+  int n{static_cast<int>(g.size())};
+
+  std::vector<int> disc(n, -1), low(n, -1), par(n, -1);
+  std::stack<Edge> stack{};
+
   BCC bcc{};
-  bcc.emplace(Edges{{g[0][0], g[0][1]}, {g[1][0], g[1][1]}});
+  for (int i{0}; i < n; ++i)
+  {
+    if (disc[i] == -1)
+    {
+      DFS(g, i, low, disc, par, stack, bcc);
+      bcc.insert(make_edges(stack, {-1, -1}));
+    }
+  }
 
   return bcc;
 }
