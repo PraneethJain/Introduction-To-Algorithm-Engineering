7f6889717fcbb46ac6925da1f0533378228b20bb
PraneethJain <praneethjain005@gmail.com>
2024-04-13 10:05:47 +0530

    feat: add bridges count

--- Project/truth.py
+++ Project/truth.py
@@ -6,7 +6,8 @@ def main() -> None:
     with open(input("input graph path: "), "r") as f:
         edges = [tuple(map(int, line.strip().split(" "))) for line in f.readlines()][1:]
     g.add_edges_from(edges)
-    print(len(list(nx.biconnected_components(g))))
+    print(f"Biconnected Components: {len(list(nx.biconnected_components(g)))}")
+    print(f"Bridges: {len(list(nx.bridges(g)))}")
 
 
 
