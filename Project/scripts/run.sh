ef65f45942f257b89f3dbec64be3eb9422026f67
PraneethJain <praneethjain005@gmail.com>
2024-04-24 19:34:38 +0530

    feat: output in one file

--- Project/scripts/run.sh
+++ Project/scripts/run.sh
@@ -7,9 +7,12 @@ k=$((1000*FACTOR))
 make clean
 make
 
+rm "outputs/${FACTOR}"
+
 for ((n=(10*FACTOR); 2*n<$k; n+=(10*FACTOR)))
 do
   m=$((k - n))
   echo "$n $m"
-  ./main "input_graphs/genrang/${n}_${m}.txt" > "outputs/${n}_${m}.txt"
+  echo "$n $m" >> "outputs/${FACTOR}.txt"
+  ./main "input_graphs/genrang/${n}_${m}.txt" >> "outputs/${FACTOR}.txt"
 done
