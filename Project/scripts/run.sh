dda0af4664540a937c4031228d092d696e85fe00
PraneethJain <praneethjain005@gmail.com>
2024-04-24 19:23:49 +0530

    feat: outputs

--- Project/scripts/run.sh
+++ Project/scripts/run.sh
@@ -11,5 +11,5 @@ for ((n=(10*FACTOR); 2*n<$k; n+=(10*FACTOR)))
 do
   m=$((k - n))
   echo "$n $m"
-  ./main "input_graphs/genrang/${n}_${m}.txt"
+  ./main "input_graphs/genrang/${n}_${m}.txt" > "outputs/${n}_${m}.txt"
 done
