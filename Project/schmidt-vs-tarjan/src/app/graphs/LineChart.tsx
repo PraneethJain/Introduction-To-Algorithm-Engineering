7e5887b73367cf262d211f000e9e316d820a958a
PraneethJain <praneethjain005@gmail.com>
2024-04-25 20:29:53 +0530

    refactor: graph drawing

--- Project/schmidt-vs-tarjan/src/app/graphs/LineChart.tsx
+++ Project/schmidt-vs-tarjan/src/app/graphs/LineChart.tsx
@@ -1,8 +1,7 @@
 "use client";
 import { TimeData } from "@/lib";
-import { time } from "console";
 import * as d3 from "d3";
-import { FC, useEffect, useRef } from "react";
+import { FC, useEffect } from "react";
 
 interface LineChartProps {
   graphTimes: TimeData[];
@@ -10,7 +9,7 @@ interface LineChartProps {
 
 type timeType = { time: number };
 
-export const LineChart: FC<LineChartProps> = ({ graphTimes }) => {
+const LineChart: FC<LineChartProps> = ({ graphTimes }) => {
   useEffect(() => {
     // get the array for densities
     let graphDensities: number[] = [];
@@ -23,8 +22,7 @@ export const LineChart: FC<LineChartProps> = ({ graphTimes }) => {
     let tarjanTimes: timeType[] = [],
       schmidtCheckTimes: timeType[] = [],
       schmidtFindTimes: timeType[] = [],
-      schmidtTimes: timeType[] = [],
-      allTimes: timeType[] = [];
+      schmidtTimes: timeType[] = [];
     for (let i = 0; i < graphTimes.length; ++i) {
       let curData = graphTimes[i];
       tarjanTimes.push({ time: curData.tarjanTime });
@@ -79,117 +77,44 @@ export const LineChart: FC<LineChartProps> = ({ graphTimes }) => {
 
     const lineGenerator = d3
       .line<timeType>()
-      .x((d: timeType, i) => xScale(graphDensities[i]))
+      .x((_, i) => xScale(graphDensities[i]))
       .y((d: timeType) => yScale(d.time));
 
     // transition
     const transition = d3.transition().ease(d3.easeSin).duration(2000);
 
+    const appendPath = (times: timeType[], color: string) => {
+      let path = g
+        .append("path")
+        .datum<timeType[]>(times)
+        .attr("stroke", color)
+        .attr("stroke-width", 3)
+        .attr("fill", "none")
+        .attr("d", lineGenerator)
+        .attr("transform", `translate(${margin.left},0)`);
+      let length = path.node()!.getTotalLength();
+      path
+        .attr("stroke-dashoffset", length)
+        .attr("stroke-dasharray", length)
+        .transition(transition)
+        .attr("stroke-dashoffset", 0);
+      g.selectAll("dots")
+        .data(times)
+        .enter()
+        .append("circle")
+        .attr("cx", (_, i) => xScale(graphDensities[i]) + margin.left)
+        .attr("cy", (d) => yScale(d.time))
+        .attr("r", 3)
+        .attr("fill", color);
+    };
+
     // append paths
-    let tarjanPath = g
-      .append("path")
-      .datum<timeType[]>(tarjanTimes)
-      .attr("stroke", "black")
-      .attr("stroke-width", 3)
-      .attr("fill", "none")
-      .attr("d", lineGenerator)
-      .attr("transform", `translate(${margin.left},0)`);
-    let tarjanLength = tarjanPath.node()?.getTotalLength();
-    tarjanPath
-      .attr("stroke-dashoffset", tarjanLength as number)
-      .attr("stroke-dasharray", tarjanLength as number)
-      .transition(transition)
-      .attr("stroke-dashoffset", 0);
-    g.selectAll("dots")
-      .data(tarjanTimes)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d, i) {
-        return xScale(graphDensities[i]) + margin.left;
-      })
-      .attr("cy", function (d) {
-        return yScale(d.time);
-      })
-      .attr("r", 3)
-      .attr("fill", "black");
-    let schmidtCheckPath = g
-      .append("path")
-      .datum<timeType[]>(schmidtCheckTimes)
-      .attr("stroke", "red")
-      .attr("stroke-width", 3)
-      .attr("fill", "none")
-      .attr("d", lineGenerator)
-      .attr("transform", `translate(${margin.left},0)`);
-    let schmidtCheckLength = schmidtCheckPath.node()?.getTotalLength();
-    schmidtCheckPath
-      .attr("stroke-dashoffset", schmidtCheckLength as number)
-      .attr("stroke-dasharray", schmidtCheckLength as number)
-      .transition(transition)
-      .attr("stroke-dashoffset", 0);
-    g.selectAll("dots")
-      .data(schmidtCheckTimes)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d, i) {
-        return xScale(graphDensities[i]) + margin.left;
-      })
-      .attr("cy", function (d) {
-        return yScale(d.time);
-      })
-      .attr("r", 3)
-      .attr("fill", "red");
-    let schmidtFindPath = g
-      .append("path")
-      .datum<timeType[]>(schmidtFindTimes)
-      .attr("stroke", "blue")
-      .attr("stroke-width", 3)
-      .attr("fill", "none")
-      .attr("d", lineGenerator)
-      .attr("transform", `translate(${margin.left},0)`);
-    let schmidtFindLength = schmidtFindPath.node()?.getTotalLength();
-    schmidtFindPath
-      .attr("stroke-dashoffset", schmidtFindLength as number)
-      .attr("stroke-dasharray", schmidtFindLength as number)
-      .transition(transition)
-      .attr("stroke-dashoffset", 0);
-    g.selectAll("dots")
-      .data(schmidtFindTimes)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d, i) {
-        return xScale(graphDensities[i]) + margin.left;
-      })
-      .attr("cy", function (d) {
-        return yScale(d.time);
-      })
-      .attr("r", 3)
-      .attr("fill", "blue");
-    let schmidtPath = g
-      .append("path")
-      .datum<timeType[]>(schmidtTimes)
-      .attr("stroke", "green")
-      .attr("stroke-width", 3)
-      .attr("fill", "none")
-      .attr("d", lineGenerator)
-      .attr("transform", `translate(${margin.left},0)`);
-    let schmidtLength = schmidtPath.node()?.getTotalLength();
-    schmidtPath
-      .attr("stroke-dashoffset", schmidtLength as number)
-      .attr("stroke-dasharray", schmidtLength as number)
-      .transition(transition)
-      .attr("stroke-dashoffset", 0);
-    g.selectAll("dots")
-      .data(schmidtTimes)
-      .enter()
-      .append("circle")
-      .attr("cx", function (d, i) {
-        return xScale(graphDensities[i]) + margin.left;
-      })
-      .attr("cy", function (d) {
-        return yScale(d.time);
-      })
-      .attr("r", 3)
-      .attr("fill", "green");
+    appendPath(tarjanTimes, "black");
+    appendPath(schmidtCheckTimes, "red");
+    appendPath(schmidtFindTimes, "blue");
+    appendPath(schmidtTimes, "green");
   }, [graphTimes]);
   return <svg height={"80vh"} width={"80vw"} id="linechart"></svg>;
 };
+
+export { LineChart };
