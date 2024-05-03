48f162257e18e66653cea8d09962019f8d8448c3
PraneethJain <praneethjain005@gmail.com>
2024-05-03 12:58:15 +0530

    feat: run complete graphs

--- Project/scripts/run.sh
+++ Project/scripts/run.sh
@@ -6,10 +6,20 @@ k=1000000
 make clean
 make
 
-rm "outputs/${k}.txt"
+rm "outputs/genrang/${k}.txt"
+rm "outputs/complete_graphs/data.txt"
+
+mkdir -p "outputs/genrang"
+mkdir -p "outputs/complete_graphs"
 
 for file in "input_graphs/genrang/${k}"/*
 do
   echo $file
-  ./main "${file}" >> "outputs/${k}.txt"
+  ./main "${file}" >> "outputs/genrang/${k}.txt"
+done
+
+for file in "input_graphs/complete_graphs"/*
+do
+  echo $file
+  ./main "${file}" >> "outputs/complete_graphs/data.txt"
 done
