eb7217b0f065243c9acabf0c1f458a3de522a55d
devika-bej <devika.bej@research.iiit.ac.in>
2024-05-04 03:37:47 +0530

    added more input for complete graphs

--- Project/scripts/gen_complete.py
+++ Project/scripts/gen_complete.py
@@ -1,7 +1,7 @@
 from os import system as sh
 
 sh(f"mkdir -p input_graphs/complete_graphs")
-for n in range(100, 1100, 100):
+for n in range(100, 10100, 100):
     sh(
-        f"genrang -P1 -g {n} 1 | showg -e | sed '1,2d' > input_graphs/complete_graphs/{n:04}.txt"
+        f"genrang -P1 -g {n} 1 | showg -e | sed '1,2d' > input_graphs/complete_graphs/{n:05}.txt"
     )
