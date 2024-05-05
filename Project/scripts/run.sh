8f55ed18c84749a276fa2acdf69722886dc7e240
PraneethJain <praneethjain005@gmail.com>
2024-05-05 18:27:29 +0530

    fix: density graphs naming

--- Project/scripts/run.sh
+++ Project/scripts/run.sh
@@ -1,33 +1,34 @@
 #!/usr/bin/bash
 
 
-k=1000000
+k=10000000
 
 make clean
 make
 
-# rm "outputs/genrang/${k}.txt"
-# rm "outputs/complete_graphs/data.txt"
+rm "outputs/density/${k}.txt"
+rm "outputs/complete_graphs/data.txt"
 rm "outputs/bridge_graphs/data.txt"
 
-# mkdir -p "outputs/genrang"
-# mkdir -p "outputs/complete_graphs"
+mkdir -p "outputs/density"
+mkdir -p "outputs/complete_graphs"
 mkdir -p "outputs/bridge_graphs"
-
-# for file in "input_graphs/genrang/${k}"/*
-# do
-#   echo $file
-#   ./main "${file}" >> "outputs/genrang/${k}.txt"
-# done
-
-# for file in "input_graphs/complete_graphs"/*
-# do
-#   echo $file
-#   ./main "${file}" >> "outputs/complete_graphs/data.txt"
-# done
 
 for file in "input_graphs/bridge_graphs"/*
 do
   echo $file
   ./main "${file}" >> "outputs/bridge_graphs/data.txt"
 done
+
+for file in "input_graphs/density/${k}"/*
+do
+  echo $file
+  ./main "${file}" >> "outputs/density/${k}.txt"
+done
+
+for file in "input_graphs/complete_graphs"/*
+do
+  echo $file
+  ./main "${file}" >> "outputs/complete_graphs/data.txt"
+done
+
