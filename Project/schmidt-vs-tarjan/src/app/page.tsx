8f60088a81374391b4177b880b2995186a397608
PraneethJain <praneethjain005@gmail.com>
2024-05-06 10:35:59 +0530

    feat: algorithm descriptions page

--- Project/schmidt-vs-tarjan/src/app/page.tsx
+++ Project/schmidt-vs-tarjan/src/app/page.tsx
@@ -5,7 +5,7 @@ import { Poppins } from "next/font/google";
 import Image from "next/image";
 
 const poppins = Poppins({
-  weight: ["100", "200", "500", "900"],
+  weight: ["100", "200", "300", "400", "500", "900"],
   subsets: ["latin"],
 });
 
@@ -22,7 +22,7 @@ export default async function Home() {
           " grid justify-items-center bg-black text-white text-justify"
         }
       >
-        <div className="h-screen flex flex-col justify-center items-center text-center">
+        <div className="min-h-screen flex flex-col justify-center items-center text-center">
           <h1 className="text-8xl">
             Introduction to <br /> Algorithm Engineering
           </h1>
@@ -35,6 +35,18 @@ export default async function Home() {
             <h3>Moida Praneeth Jain</h3>
           </div>
         </div>
+
+        <div className="min-h-screen">
+          <h2 className="text-6xl font-medium mb-6">Problem Description</h2>
+          <p className="text-xl">Insert problem description here</p>
+
+          <h2 className="text-6xl font-medium my-12">Implementation Details</h2>
+          <h3 className="text-5xl my-6">Jens Schmidt Algorithm</h3>
+          <p className="text-xl">Insert description here</p>
+          <h3 className="text-5xl my-6">Tarjan Hopcroft Algorithm</h3>
+          <p className="text-xl">Insert description here</p>
+        </div>
+
         <Switcher
           timeDatas={densityTimes}
           header="Increasing Density with Constant N + M"
